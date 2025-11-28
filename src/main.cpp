dcdb4ddd70438dfbfe8841b439fa084d301da7dc
krishnaoza01 <krishnaoza97@gmail.com>
2025-11-28 02:15:07 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,52 @@
-// temp file
+#include <iostream>
+#include "TrafficLightController.hpp"
+#include "HardwareAbstraction.hpp"
+#include "PedestrianButton.hpp"
+#include "EmergencySensor.hpp"
+#include "NightMode.hpp"
+#include "Timer.hpp"
+
+int main() {
+    LED redLED, yellowLED, greenLED;
+    PedestrianButton pedestrianButton;
+    EmergencySensor emergencySensor;
+    NightMode nightMode(yellowLED);
+
+    TrafficLightController controller(
+        redLED, yellowLED, greenLED,
+        pedestrianButton,
+        emergencySensor,
+        nightMode
+    );
+
+    controller.setup();
+
+    std::cout << "Traffic Light FSM Started.\n";
+    std::cout << "======================================\n";
+    std::cout << "Press ENTER anytime to simulate pedestrian button.\n";
+    std::cout << "Type 'e' + ENTER to simulate emergency vehicle.\n";
+    std::cout << "======================================\n";
+
+    std::thread inputThread([&]() {
+        while (true) {
+            std::string user;
+            std::getline(std::cin, user);
+
+            if (user == "e") {
+                std::cout << "[USER] Emergency triggered!\n";
+                emergencySensor.trigger();
+            } else {
+                std::cout << "[USER] Pedestrian button pressed!\n";
+                pedestrianButton.press();
+            }
+        }
+    });
+
+    inputThread.detach();
+
+    while (true) {
+        controller.runCycle();
+    }
+
+    return 0;
+}
